12f156f6340b308f80ba6e1ed12c9b4968f525f6
Yuri Sato <yuri.sato@stoom.com.br>
2022-10-17 21:37:24 -0300

    refactor: Mudança de planos no projeto, ler comentário em App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
-import Sheet from './src/components/Sheet';
+import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
+/*
+PAROU AQUI 17/10/2022 21:21
+Uma vez que o app web em react estiver pronto, basta subí-lo em algum servidor online e colocar o endereço no lugar deste aqui em baixo.
+Use o comando "eas build -p android --profile preview" para compilar um apk no cloud da plataforma Expo.
+React Native é horrível, puta merda. Parece que, em comparação com codar app web, eu tô amarrado sem perna nem braço, com uma maçã na boca tentando atirar de SMG com a orelha.
+*/
 export default function App() {
   return (
-    <Sheet />
+    <>
+      <View></View>
+      <WebView source={{ uri: "https://fichasop.com/" }}/> 
+    </>
   );
 }
 
